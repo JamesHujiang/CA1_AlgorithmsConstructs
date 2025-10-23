1f8d632b45763cc48b5b07d4010b681e0303caa8
JamesHujiang <50496217+JamesHujiang@users.noreply.github.com>
2025-10-23 15:59:48 +0100

    Initial project setup: CA1_AlgorithmsConstructs - Food Storage System

---
+++ src/ca1_algorithmsconstructs/CA1_AlgorithmsConstructs.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca1_algorithmsconstructs;
+
+/**
+ *
+ * @author james
+ */
+public class CA1_AlgorithmsConstructs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
